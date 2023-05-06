75231aee47bebe2b9d17daf5a35ccb49669a8f2c
hutao <hutao@vip.qq.com>
2023-05-06 16:24:55 +0800

    对话历史记忆5次

--- ai/chat.py
+++ ai/chat.py
@@ -11,7 +11,14 @@ def chat_with_openai(user_id, message, history):
     history.insert(0, {"role": "system", "content": f"{message}", "source": f"{user_id}"})
 
     # 生成 OpenAI 输入消息列表
-    openai_messages = [{"role": msg["role"], "content": msg["content"]} for msg in reversed(history)]
+    # openai_messages = [{"role": msg["role"], "content": msg["content"]} for msg in reversed(history)]
+    max_iterations = 5  # 设置最大迭代次数
+
+    openai_messages = []
+    for index, msg in enumerate(reversed(history)):
+        if index >= max_iterations:
+            break
+        openai_messages.append({"role": msg["role"], "content": msg["content"]})
 
     # 调用 ChatOpenAI API
     response = openai.ChatCompletion.create(
